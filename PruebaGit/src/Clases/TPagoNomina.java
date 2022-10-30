af22b65b216cd3cd9ac2ae8dd9ea4eb9edb5096f
HectorFC67 <99742938+HectorFC67@users.noreply.github.com>
2022-10-30 18:48:13 +0100

    Add files via upload

---
+++ PruebaGit/src/Clases/TPagoNomina.java
@@ -0,0 +1,50 @@
+package Clases;
+
+public class TPagoNomina {
+	
+	private int aCodNomina;
+	private double aValorMensual;
+	private int aCodEmpleadoFK;
+	
+	
+	public TPagoNomina() {}
+	
+	
+	/**
+	 * @return the aCodNomina
+	 */
+	public int getCodNomina() {
+		return aCodNomina;
+	}
+	/**
+	 * @param aCodNomina the aCodNomina to set
+	 */
+	public void setCodNomina(int aCodNomina) {
+		this.aCodNomina = aCodNomina;
+	}
+	/**
+	 * @return the aValorMensual
+	 */
+	public double getValorMensual() {
+		return aValorMensual;
+	}
+	/**
+	 * @param aValorMensual the aValorMensual to set
+	 */
+	public void setValorMensual(double aValorMensual) {
+		this.aValorMensual = aValorMensual;
+	}
+	/**
+	 * @return the aCodEmpleadoFK
+	 */
+	public int getCodEmpleadoFK() {
+		return aCodEmpleadoFK;
+	}
+	/**
+	 * @param aCodEmpleadoFK the aCodEmpleadoFK to set
+	 */
+	public void setCodEmpleadoFK(int aCodEmpleadoFK) {
+		this.aCodEmpleadoFK = aCodEmpleadoFK;
+	}
+
+}
