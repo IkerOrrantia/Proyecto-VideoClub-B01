4070a42438ec79fd74fd45b945dce1dafcc3e149
Iker Orrantia <iker.orrantia@opendeusto.es>
2022-11-06 18:06:59 +0100

    Cracion clase cliente

---
+++ PruebaGit/src/Clases/Cliente.java
@@ -0,0 +1,24 @@
+package Clases;
+
+public class Cliente extends Cuenta {
+	// Variable
+	private int NumerodePeliculas;
+	private Genero Genero;
+	// Anyadir numero de peliculas por tipo ---> For TipoPelicula...
+	public Cliente(String usuario, String nombre, String apellidos, String dNI, String correo, String contrasenya,
+			int telefono, String direccion) {
+		super(usuario, nombre, apellidos, dNI, correo, contrasenya, telefono, direccion);
+		this.NumerodePeliculas = NumerodePeliculas;
+	}
+	public int getNumerodePeliculas() {
+		return NumerodePeliculas;
+	}
+	public void setNumerodePeliculas(int numerodePeliculas) {
+		NumerodePeliculas = numerodePeliculas;
+	}
+	@Override
+	public String toString() {
+		return "Cliente [NumerodePeliculas=" + NumerodePeliculas + "]";
+	}
+	
+}
