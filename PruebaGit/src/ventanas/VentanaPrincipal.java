c0c316cffe3aaa6ee43fd08a93baaebd7b090e98
HectorFC67 <99742938+HectorFC67@users.noreply.github.com>
2022-12-11 19:26:06 +0100

    Mejoras VentanaPrincipal
    Casi finalizada la carga de la base de datos a la tabla para mostrarla en pantalla e iniciada la funcion de filtrar busqueda con el comboBox

--- PruebaGit/src/ventanas/VentanaPrincipal.java
+++ PruebaGit/src/ventanas/VentanaPrincipal.java
@@ -1,6 +1,13 @@
 package ventanas;
 
 import java.awt.*;
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -34,16 +41,21 @@ public class VentanaPrincipal extends JFrame{
 
 	    campoBuscador = new JTextField(20);
 	    filtros = new JComboBox();
+	    JButton aceptarBusqueda = new JButton("Filtrar");
+	    
 	    filtros.addItem("ID");
 	    filtros.addItem("Titulo");
 	    filtros.addItem("Director");
 	    filtros.addItem("Precio");
 	    filtros.addItem("Genero");
 	    filtros.addItem("Anyo");
+	    
 	    panelFiltros.add(campoBuscador);
 	    panelFiltros.add(filtros);
+	    panelFiltros.add(aceptarBusqueda);
 
-	    // creación de tabla de productos
+	    
+	    // creacion de tabla de productos
 	    String[] columnas = {"ID", "Titulo", "Director", "Genero", "Anyo", "Precio"};
 	    String[][] datos = {
 	        {"1", "Pelicula 1", "Director 1", "Genero 1", "2001", "10.00"},
@@ -52,6 +64,38 @@ public class VentanaPrincipal extends JFrame{
 	        {"4", "Pelicula 4", "Director 4", "Genero 4", "2004", "40.00"},
 	        {"5", "Pelicula 5", "Director 5", "Genero 5", "2005", "50.00"}
 	    };
+	  /*  // creacion de tabla de productos
+	    // Establecemos los detalles de la conexion a la base de datos
+	    String url = "Data/Peliculas.db";
+
+	    try {
+	      // establecemos la conexión a la base de datos
+	      Connection conn = DriverManager.getConnection(url);
+
+	      // creamos una consulta SQL para obtener los datos para transferir
+	      String query = "SELECT ID, Titulo, Director, Genero, Anyo, Precio FROM Peliculas";
+	      java.sql.Statement stmt = conn.createStatement();
+	      ResultSet rs = stmt.executeQuery(query);
+
+	      // iteramos sobre los resultados de la consulta y los transferimos a la tabla
+	      while (rs.next()) {
+	        int id = rs.getInt("ID");
+	        String titulo = rs.getString("Titulo");
+	        String director = rs.getString("Director");
+	        String genero = rs.getString("Genero");
+	        int anyo = rs.getInt("Anyo");
+	        double precio = rs.getDouble("Precio");
+
+	        // codigo para insertar estos datos en la tabla
+	      }
+
+	      // cerramos la conexion
+	      conn.close();
+	    } catch (SQLException ex) {
+	      // en caso de error, imprimimos un mensaje de error en la consola
+	      System.out.println("Error al transferir los datos: " + ex.getMessage());
+	    }
+	  */
 	    tablaProductos = new JTable(datos, columnas);
 
 	    // agregar componentes al panel principal
