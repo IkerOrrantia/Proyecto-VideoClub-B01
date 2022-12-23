db4e4373be68d0b4e56f2fe9f1a7e90233e9dd09
HectorFC67 <99742938+HectorFC67@users.noreply.github.com>
2022-12-23 14:15:16 +0100

    Ventana Admin (estructura)

---
+++ PruebaGit/src/ventanas/VentanaAdmin.java
@@ -0,0 +1,67 @@
+package ventanas;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class VentanaAdmin extends JFrame{
+	// Creamos los componentes de la ventana
+    private JLabel lblTitle = new JLabel("Panel de administrador");
+    private JButton btnUsers = new JButton("Gestionar usuarios");
+    private JButton btnProducts = new JButton("Gestionar productos");
+    private JButton btnOrders = new JButton("Gestionar pedidos");
+    private JButton btnLogout = new JButton("Cerrar sesión");
+    
+    public VentanaAdmin() {
+        // Establecemos el título de la ventana
+        setTitle("Administrador");
+        // Establecemos el tamaño de la ventana
+        setSize(400, 300);
+        // Establecemos el layout de la ventana
+        setLayout(new BorderLayout());
+
+        // Creamos un panel para colocar los botones en la parte inferior de la ventana
+        JPanel panelButtons = new JPanel();
+        panelButtons.setLayout(new FlowLayout());
+        // Añadimos los botones al panel
+        panelButtons.add(btnUsers);
+        panelButtons.add(btnProducts);
+        panelButtons.add(btnOrders);
+        panelButtons.add(btnLogout);
+
+        // Añadimos los componentes a la ventana
+        add(lblTitle, BorderLayout.NORTH);
+        add(panelButtons, BorderLayout.SOUTH);
+
+        // Establecemos la acción para el botón de cerrar sesión
+        btnLogout.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Cierra la ventana y vuelve a la pantalla de inicio de sesión
+                dispose();
+                new VentanaInicioSesion().setVisible(true);
+            }
+        });
+
+        // Establecemos la acción para los demás botones (en este caso simplemente mostramos un mensaje)
+        btnUsers.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(VentanaAdmin.this, "Gestión de usuarios");
+            }
+        });
+        btnProducts.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(VentanaAdmin.this, "Gestión de productos");
+            }
+        });
+        btnOrders.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(VentanaAdmin.this, "Gestión de pedidos");
+            }
+        });
+    }
+}
