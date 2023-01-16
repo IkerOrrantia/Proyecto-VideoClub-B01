892366c4fd8a8a7f15abe19b2aac20f03398279b
HectorFC67 <99742938+HectorFC67@users.noreply.github.com>
2023-01-16 19:28:01 +0100

    Ventanas Admin (modificaciones)

--- PruebaGit/src/ventanas/VentanaGestionPeliculas.java
+++ PruebaGit/src/ventanas/VentanaGestionPeliculas.java
@@ -1,5 +1,79 @@
 package ventanas;
 
-public class VentanaGestionPeliculas {
+import java.awt.BorderLayout;
+import java.sql.SQLException;
+import java.util.List;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import bd.BD;
+import clases.Cuenta;
+import clases.Pelicula;
+
+public class VentanaGestionPeliculas extends JFrame{
+
+	private JTable tablaPeliculas;
+	private DefaultTableModel modeloTabla;
+	private JButton btnEliminar;
+	private List<Pelicula> listaPeliculas;
+	private BD bd;
+	private Pelicula peliculaAnyadir;
+	// FALTA TRAER LOS DATOS DE USUARIOS DE LA BASE DE DATOS
+	public VentanaGestionPeliculas() throws SQLException{
+		// Configura la ventana
+		setTitle("Ventana Gestión de Usuarios");
+		setBounds(525, 250, 900, 600); // (Posicion x, Posicion Y, Anchura, Altura)
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+		// Crea el modelo de la tabla
+		modeloTabla = new DefaultTableModel();
+		modeloTabla.addColumn("ID");
+		modeloTabla.addColumn("Nombre");
+		modeloTabla.addColumn("Director");
+		modeloTabla.addColumn("Genero");
+		modeloTabla.addColumn("Anyo");
+		modeloTabla.addColumn("Precio");
+		modeloTabla.addColumn("Cantidad");
+		modeloTabla.addColumn("Descripcion");
+
+		// Crea la tabla
+		tablaPeliculas = new JTable(modeloTabla);
+
+		// Agrega la tabla a un scroll pane y lo agrega a la ventana
+		JScrollPane scrollPane = new JScrollPane(tablaPeliculas);
+		add(scrollPane);
+
+	
+		// Carga los datos de la base de datos en la tabla
+
+		setVisible(true);
+		bd = new BD();
+		try {
+			bd.connect();
+			listaPeliculas = listaPeliculas;
+			for (Pelicula p : listaPeliculas) {
+				modeloTabla.addRow(new Object[] {
+						p.getId(),
+						p.getNombre(),
+						p.getDirector(),
+						// p.getId_genero(),
+						p.getAnyo(),
+						p.getPrecio(),
+						p.getCantidad(),
+						p.getDescripcion()
+				});
+			}
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
+		// Agrega cada fila de resultados al modelo de la tabla
+	}
 }
+
--- PruebaGit/src/ventanas/VentanaGestionProductos.java
+++ PruebaGit/src/ventanas/VentanaGestionProductos.java
@@ -1,7 +1,66 @@
 package ventanas;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.event.*;
+import java.sql.SQLException;
+import java.awt.*;
 
-public class VentanaGestionProductos extends JFrame{
+public class VentanaGestionProductos extends JFrame implements ActionListener {
 
+	private JButton botonPeli, botonSerie;
+	private ImageIcon imageP, imageS;
+
+	public VentanaGestionProductos() {
+		setLayout(new FlowLayout());
+
+		botonPeli = new JButton();
+		add(botonPeli);
+		botonPeli.setSize(400,400);
+		imageP = new ImageIcon(getClass().getResource("git/Proyecto-VideoClub-B01/PruebaGit/img/peli.jpg")); // MIRAR PORQUE NO VA LA CARPETA IMG
+		botonPeli.setIcon(imageP);
+		botonPeli.addActionListener(this);
+
+		botonSerie = new JButton();
+		add(botonSerie);
+		botonSerie.setSize(400,400);
+		imageS = new ImageIcon(getClass().getResource("git/Proyecto-VideoClub-B01/PruebaGit/img/series.jpg")); // MIRAR PORQUE NO VA LA CARPETA IMG
+		botonSerie.setIcon(imageS);
+		botonSerie.addActionListener(this);
+
+
+		botonPeli.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				VentanaGestionPeliculas ventana;
+				try {
+					ventana = new VentanaGestionPeliculas();
+					ventana.setVisible(true);
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+			}
+		});
+		botonSerie.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				VentanaGestionSeries ventana = new VentanaGestionSeries();
+				ventana.setVisible(true);
+			}
+		});
+	}
+	public static void main(String[] args) {
+		VentanaGestionProductos gui = new VentanaGestionProductos();
+		gui.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		gui.pack();
+		gui.setTitle("VentanaGestionProductos");
+		gui.setVisible(true);
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
