b5c7585dce4580b0b3809dc3e283ab2a412701c7
HectorFC67 <99742938+HectorFC67@users.noreply.github.com>
2023-01-16 23:23:17 +0100

    Ventana Admin - Series

--- PruebaGit/src/ventanas/VentanaGestionSeries.java
+++ PruebaGit/src/ventanas/VentanaGestionSeries.java
@@ -1,7 +1,37 @@
 package ventanas;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.util.Arrays;
+import java.util.Vector;
 
-public class VentanaGestionSeries extends JFrame{
+public class VentanaGestionSeries extends JFrame {
+    private JTable tablaSeries;
+    private DefaultTableModel modeloDatos;
 
+    public VentanaGestionSeries() {
+        // Configuración de la ventana
+        this.setTitle("Gestión de Series");
+        this.setSize(800, 600);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Creación de la tabla
+        Vector<String> cabecera = new Vector<>(Arrays.asList("ID", "NOMBRE", "CREADOR", "TEMPORADAS", "ID GENERO", "PRECIO", "CANTIDAD", "DESCRIPCION"));
+        this.modeloDatos = new DefaultTableModel(new Vector<Vector<Object>>(), cabecera);
+        this.tablaSeries = new JTable(modeloDatos);
+
+        // Añadir la tabla a un panel con scroll
+        JPanel panelTabla = new JPanel();
+        panelTabla.add(new JScrollPane(tablaSeries));
+
+        // Añadir el panel a la ventana
+        this.add(panelTabla, BorderLayout.CENTER);
+    }
+
+    public static void main(String[] args) {
+        VentanaGestionSeries vgs = new VentanaGestionSeries();
+        vgs.setVisible(true);
+    }
 }
