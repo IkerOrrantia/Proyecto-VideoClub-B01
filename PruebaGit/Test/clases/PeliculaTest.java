ab4163810a151e5bd0c96e50b404673ef533c198
HectorFC67 <99742938+HectorFC67@users.noreply.github.com>
2023-01-17 21:04:13 +0100

    Add files via upload

--- PruebaGit/Test/clases/PeliculaTest.java
+++ PruebaGit/Test/clases/PeliculaTest.java
@@ -10,12 +10,13 @@ public class PeliculaTest {
 	private Pelicula pelicula;
 	@Before
 	public void setUp() {
-		pelicula = new Pelicula(1, "Pelicula 1", "Director 1", 2, 2010, 10.0, 10, "Ejemplo descripcion Pelicula 1", "imagen/foto1");
+		pelicula = new Pelicula(0, 0, "Pelicula 1", "Director 1", 2, 2010, 10.0, 10, "Ejemplo descripcion Pelicula 1", "imagen/foto1");
 	}
 
   @Test
   public void testConstructorConArgumentos() {
-    assertEquals(1, pelicula.getId());
+    assertEquals(0, pelicula.getId());
+    assertEquals(0, pelicula.getId_producto());
     assertEquals("Pelicula 1", pelicula.getNombre());
     assertEquals("Director 1", pelicula.getDirector());
     assertEquals(2, pelicula.getId_genero());
--- PruebaGit/Test/clases/SerieTest.java
+++ PruebaGit/Test/clases/SerieTest.java
@@ -8,7 +8,7 @@ public class SerieTest {
 	private Serie s;
 	@Before
 	public void setUp() {
-		 s = new Serie(1, "Serie 1", "Creador 1", 2010, 3, 1, 20.0, 10, "Descripción de la serie 1", "imagen/serie1");
+		 s = new Serie(0, 0, "Serie 1", "Creador 1", 1, 2010, 3, 20.0, 10, "Descripción de la serie 1", "imagen/serie1");
 	}
     @Test
    public void testConstructorConArgumentos() {
@@ -16,12 +16,13 @@ public class SerieTest {
         
         
         // Comprobamos que los valores se han inicializado correctamente
-        assertEquals(1, s.getId());
+        assertEquals(0, s.getId());
+        assertEquals(0, s.getId_producto());
         assertEquals("Serie 1", s.getNombre());
-        assertEquals("Creador 1", s.getCreador());
+        assertEquals("Creador 1", s.getDirector());
+        assertEquals(1, s.getId_genero());
         assertEquals(2010, s.getAnyo());
         assertEquals(3, s.getTemporadas());
-        assertEquals(1, s.getId_genero());
         assertEquals(20.0, s.getPrecio(), 0.001);
         assertEquals(10, s.getCantidad());
         assertEquals("Descripción de la serie 1", s.getDescripcion());
@@ -31,15 +32,16 @@ public class SerieTest {
     @Test
     public void testConstructorPorDefecto() {
         // Creamos una nueva serie usando el constructor por defecto
-        Serie serie = new Serie(-1, "Sin nombre", "", 0, 0, 0, 0.0, 0, "", "");
+        Serie serie = new Serie(0, 0, "Sin nombre", "", 0, 0, 0, 0.0, 0, "", "");
         
         // Comprobamos que los valores se han inicializado correctamente
-        assertEquals(-1, serie.getId());
+        assertEquals(0, serie.getId());
+        assertEquals(0, serie.getId_producto());
         assertEquals("Sin nombre", serie.getNombre());
-        assertEquals("", serie.getCreador());
+        assertEquals("", serie.getDirector());
+        assertEquals(0, serie.getId_genero());
         assertEquals(0, serie.getAnyo());
         assertEquals(0, serie.getTemporadas());
-        assertEquals(0, serie.getId_genero());
         assertEquals(0.0, serie.getPrecio(), 0.001);
         assertEquals(0, serie.getCantidad());
         assertEquals("", serie.getDescripcion());
@@ -49,15 +51,16 @@ public class SerieTest {
     @Test
     public void testSetters() {
         // Creamos una nueva serie usando el constructor por defecto
-        Serie s = new Serie();
+        Serie s = new Serie(0, 0, "Serie 1", "Creador 1", 3, 2010, 4, 20.0, 10, "Descripción de la serie 1", "imagen/serie1");;
         
         // Utilizamos los setters para cambiar los valores de la serie
-        s.setId(2);
+        s.setId(0);
+        s.setId_producto(0);
         s.setNombre("Serie 2");
-        s.setCreador("Creador 2");
+        s.setDirector("Creador 2");
+        s.setId_genero(2);
         s.setAnyo(2011);
         s.setTemporadas(5);
-        s.setId_genero(2);
         s.setPrecio(30.0);
         s.setCantidad(15);
         s.setDescripcion("Descripción de la serie 2");
